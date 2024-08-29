93d9f659a96133918a401a6b5fe69ab9c75552ef
caesarrimskiyyyyyyyyyyyy <caesarrimskiy@gmail.com>
2024-08-29 17:50:59 +0300

    finish

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
+        UserServiceImpl userService = new UserServiceImpl();
 
+        // create table
+        userService.createUsersTable();
+
+        // add 4 users
+        userService.saveUser("Tony", "Soprano", (byte) 56);
+        userService.saveUser("Vito", "Spatafore", (byte) 56);
+        userService.saveUser("Christopher", "Moltisanti", (byte) 56);
+        userService.saveUser("Junior", "Soprano", (byte) 56);
+
+        // getting all users
+        System.out.println(userService.getAllUsers());
+
+        // deletion of table entries
+        userService.cleanUsersTable();
+
+        // drop table
+        userService.dropUsersTable();
     }
 }
